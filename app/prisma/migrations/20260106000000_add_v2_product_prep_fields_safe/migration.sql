764a8aa98d01fda86d6f3002b160740a213eb660
stevenshelley58-afk <steven@labcast.com.au>
2026-01-06 05:54:44 +0800

    Add safe migration for v2 product prep fields (scene_role, replacement_rule, allow_space_creation)

---
+++ app/prisma/migrations/20260106000000_add_v2_product_prep_fields_safe/migration.sql
@@ -0,0 +1,20 @@
+-- Add v2 product prep fields if they don't exist
+-- This migration is safe to run even if columns already exist
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                   WHERE table_name = 'product_assets' AND column_name = 'scene_role') THEN
+        ALTER TABLE "product_assets" ADD COLUMN "scene_role" TEXT;
+    END IF;
+    
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                   WHERE table_name = 'product_assets' AND column_name = 'replacement_rule') THEN
+        ALTER TABLE "product_assets" ADD COLUMN "replacement_rule" TEXT;
+    END IF;
+    
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+                   WHERE table_name = 'product_assets' AND column_name = 'allow_space_creation') THEN
+        ALTER TABLE "product_assets" ADD COLUMN "allow_space_creation" BOOLEAN;
+    END IF;
+END $$;
